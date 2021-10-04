4204e12406579aa4133133e870b06d0f6653fbfd
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-10-04 15:34:14 +0300

    Add starter files

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Frontend Mentor | Blogr</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
         <h1>Hello World!</h1>
